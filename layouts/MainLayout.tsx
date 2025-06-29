aa0a82c33e995e34de1baccabc041c628c8e9525
Aryan <aryan.k.randeriya@gmail.com>
2025-06-30 02:19:55 +0530

    feat: enhance MainLayout to conditionally render header and footer

--- layouts/MainLayout.tsx
+++ layouts/MainLayout.tsx
@@ -3,18 +3,30 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 import { ReactNode } from "react";
 
-export default function MainLayout({ children }: { children: ReactNode }) {
+export default function MainLayout({
+  children,
+  showHeader = true,
+  showFooter = true,
+}: {
+  children: ReactNode;
+  showHeader?: boolean;
+  showFooter?: boolean;
+}) {
   return (
     <>
       <Html>
         <Tailwind>
-          <Body className="mx-auto my-auto p-7 font-sans max-w-[520px]">
-            <Header />
-            <Hr />
+          <Body className="mx-auto my-auto p-7 font-sans max-w-[500px]">
+            {showHeader && (
+              <>
+                <Header />
+                <Hr />
+              </>
+            )}
 
             <div className="mt-14 mb-8">{children}</div>
 
-            <Footer />
+            {showFooter && <Footer />}
           </Body>
         </Tailwind>
       </Html>
